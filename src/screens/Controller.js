296b64fd395e775169c9055cf09bbd5bc07b49df
nikhil18991sharmakony <nikhil.sharma@kony.com>
2020-05-18 09:58:35 +0530

    commit controller

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,24 +1,52 @@
-import React, {Component} from 'react';
-import Home from '../screens/home/Home';
-import Details from '../screens/details/Details';
-import Checkout from '../screens/checkout/Checkout';
-import Profile from '../screens/profile/Profile';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './home/Home';
+import Details from './details/Details';
+import Checkout from './checkout/Checkout';
+import Profile from './profile/Profile';
+import  PrivateRoute  from '../common/PrivateRoute';
 
 class Controller extends Component {
-    baseUrl = "http://localhost:8080/api";
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>} />
-                    <Route path='/restaurant/:restaurantID' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
-                    <Route path='/checkout' render={(props) => <Checkout {...props}baseUrl={this.baseUrl} />} />
-                    <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
-                </div>
-            </Router>
-        )
-    }
+
+  constructor(props)
+  {
+    super(props);
+    this.baseUrl = "http://localhost:8080/api/";
+    this.state = {
+      loggedIn: sessionStorage.getItem('access-token') == null ? false : true
+    };
+  }
+
+
+  render(){
+    return(
+      <Router>
+         <div className="main-container">
+           <Switch>
+              <Route exact path='/' render={(props) => <Home {...props} baseUrl = {this.baseUrl} /> }  />
+              <Route exact path='/restaurant/:id' render={(props) => <Details {...props} baseUrl = {this.baseUrl} /> }  />
+                
+              {/* Added private router for checkout page - because only login user can go that page */}
+              <PrivateRoute 
+                  exact 
+                  path='/checkout'
+                  component={Checkout} 
+                  baseUrl = {this.baseUrl}
+                  />
+              {/* Added private router for profile page - because only login user can go that page */}
+              <PrivateRoute 
+                  exact 
+                  path='/profile'
+                  component={Profile} 
+                  baseUrl = {this.baseUrl}
+                  /> 
+            </Switch>
+        </div>
+      </Router>
+
+    )
+  }
 }
 
 export default Controller;
